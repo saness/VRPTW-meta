c7bfd290371bd194f527f54de333f857797d7ce6
Sanish Suwal <sanishsuwal3@gmail.com>
2024-04-22 15:39:47 -0400

    Separated graph and customer

--- graph.py
+++ graph.py
@@ -1,17 +1,3 @@
 import numpy as np
 
-class Customer:
-    def __init__(self, id, x, y, demand, ready_time, due_date, service_time):
-        super()
-        self.id = id
-        self.x = x
-        self.y = y
-        self.demand = demand
-        self.ready_time = ready_time
-        self.due_date = due_date
-        self.service_time = service_time
 
-        if self.id == 0:
-            self.is_depot = True
-        else:
-            self.is_depot = False
