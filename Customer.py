a1ac7abffcf215c2ff7e12f8e69d086c7277d9be
Sanish Suwal <sanishsuwal3@gmail.com>
2024-04-30 20:26:00 -0400

    added comments

--- Customer.py
+++ Customer.py
@@ -1,5 +1,15 @@
 class Customer:
     def __init__(self, id, x, y, demand, ready_time, due_date, service_time):
+        """
+        Constructor for Customer class
+        :param id: customer id
+        :param x: x coordinate
+        :param y: y coordinate
+        :param demand: demand of customer
+        :param ready_time: ready time
+        :param due_date: due date of delivery
+        :param service_time: service time
+        """
         super()
         self.id = id
         self.x = x
@@ -9,6 +19,7 @@ class Customer:
         self.due_date = due_date
         self.service_time = service_time
 
+        # if id is 0 then it is a depot
         if self.id == 0:
             self.is_depot = True
         else:
