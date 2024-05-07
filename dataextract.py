7ad2e733511cd6f6851c62aa2995a749ef6d17e1
Sanish Suwal <sanishsuwal3@gmail.com>
2024-05-07 15:59:46 -0400

    moved stuff

---
+++ dataextract.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+from Customer import *
+
+def create_customer(file_path):
+    """
+    Calculates the customer and vehicle information from the file
+    :param file_path: path of the file
+    :return: returns customer and vehicle information
+    """
+    customer_list = []
+    with open(file_path, 'rt') as f:
+        count = 1
+        for line in f:
+            if count == 5:
+                vehicle_number, vehicle_capacity = line.split()
+                vehicle_number, vehicle_capacity = int(vehicle_number), int(vehicle_capacity)
+            elif count >= 10:
+                customer_list.append(line.split())
+            count += 1
+    customer_numbers = len(customer_list)
+    customers = []
+
+    create_customer_nodes(customers, customer_list)
+
+    customer_distance_matrix = np.zeros((customer_numbers, customer_numbers))
+    customer_distance_matrix = calculate_distance_matrix(customer_numbers, customer_distance_matrix, customers)
+
+    return customer_numbers, customers, customer_distance_matrix, vehicle_number, vehicle_capacity
+def calculate_distance(customer_1, customer_2):
+    """
+    Calculates distance between two customers or nodes
+    :param customer_1: first customer (C_i)
+    :param customer_2: second customer (C_j)
+    :return: distance between customers or nodes
+    """
+    return np.linalg.norm((customer_1.x_coordinate - customer_2.x_coordinate, customer_1.y_coordinate - customer_2.y_coordinate))
+
+def create_customer_nodes(customers, customer_list):
+    """
+    Adds customer details  to customer list as Customer class
+    :param customers: list of customers as Customer node
+    :param customer_list: list of extracted customer details from dataset
+    :return:
+    """
+    for customer in customer_list:
+        customers.append(Customer(int(customer[0]), float(customer[1]), float(customer[2]), float(customer[3]),
+                                  float(customer[4]), float(customer[5]), float(customer[6])))
+
+def calculate_distance_matrix(customer_numbers, customer_distance_matrix, customers):
+    """
+    Fills the distance between customers in customer distance matrix
+    :param customer_numbers: number of customers
+    :param customer_distance_matrix: customer distance matrix
+    :param customers:customers or nodes in graph
+    :return: filled customer distance matrix
+    """
+    for i in range(customer_numbers):
+        customer_1 = customers[i]
+        customer_distance_matrix[i][i] = 1e-8
+        for j in range(i + 1, customer_numbers):
+            customer_2 = customers[j]
+            customer_distance_matrix[i][j] = calculate_distance(customer_1, customer_2)
+            customer_distance_matrix[j][i] = customer_distance_matrix[i][j]
+
+    return customer_distance_matrix
--- graph.py
+++ graph.py
@@ -6,6 +6,7 @@ author: Sanish Suwal(ss4657@rit.edu), Jay Nair(an1147@rit.edu), Bhavdeep Khileri
 """
 import numpy as np
 from Customer import *
+from dataextract import *
 
 class Graph:
     def __init__(self, file_path, rho = 0.1):
@@ -18,7 +19,7 @@ class Graph:
         # pheromone evaporation rate
         self.rho = rho
         self.customer_numbers, self.customers, self.customer_distance_matrix, self.vehicle_number, self.vehicle_capacity \
-            = self.create_customer(file_path)
+            = create_customer(file_path)
         self.nearest_travel_path, self.pheromone_value = self.nearest_heuristic()
         # initial pheromone value
         self.pheromone_value = (1/self.pheromone_value * self.customer_numbers)
@@ -27,63 +28,6 @@ class Graph:
         # Closeness (n_ij)
         self.information_matrix = 1/ self.customer_distance_matrix
 
-    @staticmethod
-    def create_customer(self, file_path):
-        """
-        Calculates the customer and vehicle information from the file
-        :param file_path: path of the file
-        :return: returns customer and vehicle information
-        """
-        customer_list = []
-        with open(file_path, 'rt') as f:
-            count = 1
-            for line in f:
-                if count == 5:
-                    vehicle_number, vehicle_capacity = line.split()
-                    vehicle_number, vehicle_capacity = int(vehicle_number), int(vehicle_capacity)
-                elif count >= 10:
-                    customer_list.append(line.split())
-                count += 1
-        customer_numbers = len(customer_list)
-        customers = []
-
-        self.create_customer_nodes(customers, customer_list)
-
-        customer_distance_matrix = np.zeros((customer_numbers, customer_numbers))
-        customer_distance_matrix = Graph.calculate_distance_matrix(customer_numbers, customer_distance_matrix, customers)
-
-        return customer_numbers, customers, customer_distance_matrix, vehicle_number, vehicle_capacity
-
-    @staticmethod
-    def create_customer_nodes(customers, customer_list):
-        """
-        Adds customer details  to customer list as Customer class
-        :param customers: list of customers as Customer node
-        :param customer_list: list of extracted customer details from dataset
-        :return:
-        """
-        for customer in customer_list:
-            customers.append(Customer(int(customer[0]), float(customer[1]), float(customer[2]), float(customer[3]),
-                                      float(customer[4]), float(customer[5]), float(customer[6])))
-
-    @staticmethod
-    def calculate_distance_matrix(customer_numbers, customer_distance_matrix, customers):
-        """
-        Fills the distance between customers in customer distance matrix
-        :param customer_numbers: number of customers
-        :param customer_distance_matrix: customer distance matrix
-        :param customers:customers or nodes in graph
-        :return: filled customer distance matrix
-        """
-        for i in range(customer_numbers):
-            customer_1 = customers[i]
-            customer_distance_matrix[i][i] = 1e-8
-            for j in range(i + 1, customer_numbers):
-                customer_2 = customers[j]
-                customer_distance_matrix[i][j] = Graph.calculate_distance(customer_1, customer_2)
-                customer_distance_matrix[j][i] = customer_distance_matrix[i][j]
-
-        return customer_distance_matrix
 
     def helper_function(self, index, time, distance):
         """
@@ -185,15 +129,6 @@ class Graph:
 
         return closest_index
 
-    @staticmethod
-    def calculate_distance(customer_1, customer_2):
-        """
-        Calculates distance between two customers or nodes
-        :param customer_1: first customer (C_i)
-        :param customer_2: second customer (C_j)
-        :return: distance between customers or nodes
-        """
-        return np.linalg.norm((customer_1.x_coordinate - customer_2.x_coordinate, customer_1.y_coordinate - customer_2.y_coordinate))
 
     def update_local_pheromone(self, i, j):
         """
